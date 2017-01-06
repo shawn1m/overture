04c95c76d1382aebdd12141e6a59b9beec632846
shawn1m <shawn1m@users.noreply.github.com>
2017-01-06 19:54:08 +0800

    Revise build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir -p release
+wget https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt
+wget https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt
+cp config.sample.json config.json
 
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o release/overture-darwin-amd64 main.go
-GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o release/overture-linux-386 main.go
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o release/overture-windows-amd64.exe main.go
-GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -o release/overture-windows-386.exe main.go
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o overture-darwin-amd64 main.go
+GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o overture-linux-386 main.go
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o overture-windows-amd64.exe main.go
+GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -o overture-windows-386.exe main.go
 
-zip release/overture-darwin-amd64.zip release/overture-darwin-amd64
-zip release/overture-linux-386.zip release/overture-linux-386
-zip release/overture-windows-amd64.zip release/overture-windows-amd64.exe
-zip release/overture-windows-386.zip release/overture-windows-386.exe
+zip overture-darwin-amd64.zip overture-darwin-amd64 china_ip_list.txt gfwlist.txt config.json
+zip overture-linux-386.zip overture-linux-386 china_ip_list.txt gfwlist.txt config.json
+zip overture-windows-amd64.zip overture-windows-amd64.exe china_ip_list.txt gfwlist.txt config.json
+zip overture-windows-386.zip overture-windows-386.exe china_ip_list.txt gfwlist.txt config.json
