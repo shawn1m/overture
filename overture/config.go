4b4708b232e53664f2e1ed7f6c392b6d913c6fa2
holyshawn <holyshawn@outlook.com>
2017-01-14 22:21:18 +0800

    Add edns0 subnet config, min-ttl config, revise code

--- overture/config.go
+++ overture/config.go
@@ -7,37 +7,40 @@ import (
 	"os"
 )
 
-var Config *configType
-
-type dnsServer struct{
-	Address    string
-	Method     string
+type dnsServer struct {
+	Address  string
+	Protocol string
 }
 
 type jsonType struct {
-	BindAddress           string
-	PrimaryDNSAddress     string
-	PrimaryDNSMethod      string
-	AlternativeDNSAddress string
-	AlternativeDNSMethod  string
-	Timeout               int
-	RedirectIPv6Record    bool
-	IPNetworkFilePath     string
-	DomainFilePath        string
-	DomainBase64Decode    bool
+	BindAddress            string
+	PrimaryDNSAddress      string
+	PrimaryDNSProtocol     string
+	AlternativeDNSAddress  string
+	AlternativeDNSProtocol string
+	Timeout                int
+	RedirectIPv6Record     bool
+	IPNetworkFilePath      string
+	DomainFilePath         string
+	DomainBase64Decode     bool
+	MinimalTTL             int
+	EDNSClientSubnetPolicy string
+	EDNSClientSubnetIP     string
 }
 
 type configType struct {
-	BindAddress           string
-	PrimaryDNSServer      dnsServer
-	AlternativeDNSServer  dnsServer
-	Timeout               int
-	RedirectIPv6Record    bool
-	IPNetworkFilePath     string
-	DomainFilePath        string
-	DomainBase64Decode    bool
+	BindAddress            string
+	PrimaryDNSServer       dnsServer
+	AlternativeDNSServer   dnsServer
+	Timeout                int
+	RedirectIPv6Record     bool
+	IPNetworkFilePath      string
+	DomainFilePath         string
+	DomainBase64Decode     bool
+	MinimalTTL             int
+	EDNSClientSubnetPolicy string
+	EDNSClientSubnetIP     string
 }
-
 
 func parseJson(path string) *jsonType {
 
@@ -64,24 +67,27 @@ func parseJson(path string) *jsonType {
 	return result
 }
 
-func ParseConfig(path string) *configType {
+func parseConfig(path string) *configType {
 
 	json_result := parseJson(path)
 	config := &configType{
 		BindAddress: json_result.BindAddress,
 		PrimaryDNSServer: dnsServer{
-			Address: json_result.PrimaryDNSAddress,
-			Method: json_result.PrimaryDNSMethod,
+			Address:  json_result.PrimaryDNSAddress,
+			Protocol: json_result.PrimaryDNSProtocol,
 		},
 		AlternativeDNSServer: dnsServer{
-			Address: json_result.AlternativeDNSAddress,
-			Method: json_result.AlternativeDNSMethod,
+			Address:  json_result.AlternativeDNSAddress,
+			Protocol: json_result.AlternativeDNSProtocol,
 		},
-		Timeout: json_result.Timeout,
-		RedirectIPv6Record: json_result.RedirectIPv6Record,
-		IPNetworkFilePath: json_result.IPNetworkFilePath,
-		DomainFilePath: json_result.DomainFilePath,
-		DomainBase64Decode: json_result.DomainBase64Decode,
+		Timeout:                 json_result.Timeout,
+		RedirectIPv6Record:      json_result.RedirectIPv6Record,
+		IPNetworkFilePath:       json_result.IPNetworkFilePath,
+		DomainFilePath:          json_result.DomainFilePath,
+		DomainBase64Decode:      json_result.DomainBase64Decode,
+		MinimalTTL:              json_result.MinimalTTL,
+		EDNSClientSubnetPolicy:  json_result.EDNSClientSubnetPolicy,
+		EDNSClientSubnetIP: json_result.EDNSClientSubnetIP,
 	}
 
 	return config
