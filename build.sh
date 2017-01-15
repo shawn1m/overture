d50ec180b98d26d051f6021dec87fd8f4cf2828b
V.E.O <veo@live.com>
2017-01-15 16:01:45 +0800

    support tcp dns

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wget https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt
-wget https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt
+wget -N https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt
+wget -N https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt
 cp config.sample.json config.json
 
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o overture-darwin-amd64 main.go
