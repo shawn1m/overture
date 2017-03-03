0aabdf373ac576c2a476963b9881dd831f20f415
shawn1m <shawn1m@users.noreply.github.com>
2017-03-03 15:41:36 +0800

    Fix build.py ".exe" problem from the idea of @everfly

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 
 import subprocess
 
-go_os_arch_list = [
+GO_OS_ARCH_LIST = [
     ["darwin", "amd64"],
     ["linux", "386"],
     ["linux", "amd64"],
@@ -16,29 +16,30 @@ go_os_arch_list = [
     ["windows", "amd64"]
               ]
 
-china_ip_list_dict = {"name": "china_ip_list.txt",
+CHINA_IP_LIST_DICT = {"name": "china_ip_list.txt",
                       "url": "https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt"}
-gfwlist_dict = {"name": "gfwlist.txt",
+GFWLIST_DICT = {"name": "gfwlist.txt",
                 "url": "https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt"}
 
 if __name__ == "__main__":
 
     subprocess.check_call("cp config.sample.json config.json", shell=True)
 
-    for url in [china_ip_list_dict["url"], gfwlist_dict["url"]]:
+    for url in [CHINA_IP_LIST_DICT["url"], GFWLIST_DICT["url"]]:
         try:
             subprocess.check_call("wget -N " + url, shell=True)
         except subprocess.CalledProcessError:
             print("Get " + url + " failed.")
 
-    for o, a in go_os_arch_list:
-        name = "overture-" + o + "-" + a
+    for o, a in GO_OS_ARCH_LIST:
+        zip_name = "overture-" + o + "-" + a
+        binary_name = zip_name + (".exe" if o == "windows" else "")
         version = subprocess.check_output("git describe --tags", shell=True).decode()
         try:
             subprocess.check_call("GOOS=" + o + " GOARCH=" + a + " CGO_ENABLED=0" +
-                                  " go build -ldflags " + "\"-X main.version=" + version + "\" -o " + name + " main/main.go", shell=True)
-            subprocess.check_call("zip " + name + ".zip " +
-                                  name + " " + china_ip_list_dict["name"] + " " +
-                                  gfwlist_dict["name"] + " hosts config.json", shell=True)
+                                  " go build -ldflags " + "\"-X main.version=" + version + "\" -o " + binary_name + " main/main.go", shell=True)
+            subprocess.check_call("zip " + zip_name + ".zip " +
+                                  binary_name + " " + CHINA_IP_LIST_DICT["name"] + " " +
+                                  GFWLIST_DICT["name"] + " hosts config.json", shell=True)
         except subprocess.CalledProcessError:
             print(o + " " + a + " failed.")
