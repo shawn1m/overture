f57e9a9d21d991a8b5823ce73007e1bad7a55af4
NyaMisty <gyc990326@gmail.com>
2020-02-22 19:25:54 +0800

    Support build android and ios executables (#212)
    Mobile build with test
    
    Change to ubuntu + mac
    
    Fix build.py
    
    Fix android build
    
    Cleanup the appveyor.yml

--- build.py
+++ build.py
@@ -21,17 +21,41 @@ GO_OS_ARCH_LIST = [
     ["windows", "amd64"]
               ]
 
+GO_IOS_ARCH_LIST = [
+    ["darwin", "arm64"],
+    ["darwin", "arm"]
+]
 
-def go_build_zip():
+GO_ANDROID_ARCH_LIST = [
+    ["android", "arm", "arm-linux-androideabi"],
+    ["android", "arm64", "aarch64-linux-android"],
+    ["android", "386", "i686-linux-android"],
+    ["android", "amd64", "x86_64-linux-android"],
+]
+
+
+def go_build_desktop(binary_name, version, o, a, p):
+    mipsflag = (" GOMIPS=" + (p[0] if p else "") if p else "")
+    subprocess.check_call("GOOS=" + o + " GOARCH=" + a + mipsflag + " CGO_ENABLED=0" + " go build -ldflags \"-s -w " +
+                                  "-X main.version=" + version + "\" -o " + binary_name + " main/main.go", shell=True)
+
+def go_build_ios(binary_name, version, o, a, p):
+    subprocess.check_call("CC=$(go env GOROOT)/misc/ios/clangwrap.sh GOOS=" + o + " GOARCH=" + a + " CGO_ENABLED=1" + " go build -ldflags \"-s -w " +
+                                  "-X main.version=" + version + "\" -o " + binary_name + " main/main.go", shell=True)
+
+def go_build_android(binary_name, version, o, a, p):
+    triple = p[0]
+    subprocess.check_call("CC=$ANDROID_NDK_ROOT/bin/" + triple + "/bin/clang GOOS=" + o + " GOARCH=" + a + " CGO_ENABLED=1" + " go build -ldflags \"-s -w " +
+                                  "-X main.version=" + version + "\" -o " + binary_name + " main/main.go", shell=True)
+
+def go_build_zip(arches, builder):
     subprocess.check_call("GOOS=windows go get -v github.com/shawn1m/overture/main", shell=True)
-    for o, a, *p in GO_OS_ARCH_LIST:
+    for o, a, *p in arches:
         zip_name = "overture-" + o + "-" + a + ("-" + (p[0] if p else "") if p else "")
         binary_name = zip_name + (".exe" if o == "windows" else "")
         version = subprocess.check_output("git describe --tags", shell=True).decode()
-        mipsflag = (" GOMIPS=" + (p[0] if p else "") if p else "")
         try:
-            subprocess.check_call("GOOS=" + o + " GOARCH=" + a + mipsflag + " CGO_ENABLED=0" + " go build -ldflags \"-s -w " +
-                                  "-X main.version=" + version + "\" -o " + binary_name + " main/main.go", shell=True)
+            builder(binary_name, version, o, a, p)
             subprocess.check_call("zip " + zip_name + ".zip " + binary_name + " " + "hosts_sample "
                                                                                     "ip_network_primary_sample "
                                                                                     "ip_network_alternative_sample "
@@ -66,4 +90,10 @@ if __name__ == "__main__":
         create_sample_file()
 
     if "-build" in sys.argv:
-        go_build_zip()
+        go_build_zip(GO_OS_ARCH_LIST, go_build_desktop)
+    
+    if "-build-ios" in sys.argv:
+        go_build_zip(GO_IOS_ARCH_LIST, go_build_ios)
+    
+    if "-build-android" in sys.argv:
+        go_build_zip(GO_ANDROID_ARCH_LIST, go_build_android)
