f51edc883c2105196676a7312494814512dadacb
zhanhb <6323014+zhanhb@users.noreply.github.com>
2020-05-06 20:52:24 +0800

    Fix ipv6 cidr shoud not contain ipv4 ip address (#233)

--- core/common/ipset_test.go
+++ core/common/ipset_test.go
@@ -1,36 +1,49 @@
 package common
 
 import (
-	log "github.com/sirupsen/logrus"
 	"net"
 	"testing"
 )
 
-func init() {
-	log.SetLevel(log.TraceLevel)
+func contains(ipNets []*net.IPNet, ip net.IP) bool {
+	for _, ipNet := range ipNets {
+		if ipNet.Contains(ip) {
+			return true
+		}
+	}
+	return false
 }
 
-func TestNewIPSet(t *testing.T) {
+func TestIPSet(t *testing.T) {
 	var ipNetList []*net.IPNet
 	for _, s := range []string{
 		"::ffff:192.168.1.0/120",
 		"192.168.2.0/24",
 		"192.168.4.0/24",
+		"::fffe:0:0/95", // a range covers all IPv4-mapped Address
+		"fe80::1234:5678:9000/120",
 	} {
 		_, ipNet, _ := net.ParseCIDR(s)
 		ipNetList = append(ipNetList, ipNet)
 	}
 	ipSet := NewIPSet(ipNetList)
-	if ipSet.Contains(net.ParseIP("192.168.3.0"), true, "test1") {
-		t.Error()
-	}
-	if !ipSet.Contains(net.ParseIP("192.168.2.1"), true, "test2") {
-		t.Error()
-	}
-	if !ipSet.Contains(net.ParseIP("::ffff:192.168.2.1"), true, "test3") {
-		t.Error()
-	}
-	if !ipSet.Contains(net.ParseIP("192.168.1.1"), true, "test4") {
-		t.Error()
+
+	for _, s := range []string{
+		"192.168.3.0",
+		"192.168.2.1",
+		"::ffff:192.168.2.1",
+		"192.168.1.1",
+		"fe80::1234:5678:8fff",
+		"fe80::1234:5678:9000",
+		"fe80::1234:5678:90ff",
+		"fe80::1234:5678:9100",
+		"invalid ip",
+	} {
+		ip := net.ParseIP(s)
+		expect := contains(ipNetList, ip)
+		result := ipSet.Contains(ip, true, s)
+		if expect != result {
+			t.Errorf("expect %v, but got %v: '%v'", expect, result, s)
+		}
 	}
 }
