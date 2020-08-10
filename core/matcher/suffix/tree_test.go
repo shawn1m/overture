516c5fc106a136cbb4c1a104da9b8a1b364f992a
wzv5 <wzv5@msn.com>
2020-08-11 02:15:49 +0800

    fix suffix tree
    issue:
    When insert abc.com and 1.abc.com, abc.com will not be matched.
    
    Add an final flag to the tree to solve this problem.

--- core/matcher/suffix/tree_test.go
+++ core/matcher/suffix/tree_test.go
@@ -11,5 +11,24 @@ import (
 )
 
 func TestTree_Has(t *testing.T) {
-
+	tree := DefaultDomainTree()
+	for _, d := range []string{
+		"1.abc.com",
+		"2.abc.com",
+		"1.2.abc.com",
+	} {
+		tree.Insert(d)
+	}
+	for _, d := range []string{
+		"1.abc.com",
+		"2.abc.com",
+		"1.2.abc.com",
+	} {
+		if !tree.Has(d) {
+			t.Fail()
+		}
+	}
+	if tree.Has("abc.com") {
+		t.Fail()
+	}
 }
