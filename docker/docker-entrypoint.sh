1b8d9103f8dd443d4d48f9d314d5ec025e956c54
SgrAlpha <admin@sgr.io>
2021-07-03 02:04:06 +0800

    Fix a problem when reading env variables

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -40,23 +40,23 @@ if [ ! -z "${ALTERNATIVE_DNS}" ]; then
 fi
 
 if [ ! -z "${IP_NETWORK_FILE_PRIMARY}" ]; then
-  yq e '.ipNetworkFile.primary = "${IP_NETWORK_FILE_PRIMARY}"' -i /opt/overture/config.yml
+  yq eval '.ipNetworkFile.primary = strenv(IP_NETWORK_FILE_PRIMARY)' -i /opt/overture/config.yml
 fi
 
 if [ ! -z "${DOMAIN_FILE_ALTERNATIVE}" ]; then
-  yq e '.domainFile.alternative = "${DOMAIN_FILE_ALTERNATIVE}"' -i /opt/overture/config.yml
+  yq eval '.domainFile.alternative = strenv(DOMAIN_FILE_ALTERNATIVE)' -i /opt/overture/config.yml
 fi
 
 if [ ! -z "${HOSTS_FILE}" ]; then
-  yq e '.hostsFile.hostsFile = "${HOSTS_FILE}"' -i /opt/overture/config.yml
+  yq eval '.hostsFile.hostsFile = strenv(HOSTS_FILE)"' -i /opt/overture/config.yml
 fi
 
 if [ ! -z "${CACHE_SIZE}" ]; then
-  yq e '.cacheSize = env(CACHE_SIZE)' -i /opt/overture/config.yml
+  yq eval '.cacheSize = env(CACHE_SIZE)' -i /opt/overture/config.yml
 fi
 
 if [ ! -z "${ALTERNATIVE_DNS_SOCKS5_PROXY}" ]; then
-  yq e '.alternativeDNS.[].socks5Address = "${ALTERNATIVE_DNS_SOCKS5_PROXY}"' -i /opt/overture/config.yml
+  yq eval '.alternativeDNS.[].socks5Address = strenv(ALTERNATIVE_DNS_SOCKS5_PROXY)' -i /opt/overture/config.yml
 fi
 
 exec "$@"
